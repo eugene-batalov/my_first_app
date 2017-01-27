289685e0014bb8229ffeb30cee7a544dfd044d0c
Evgeny Batalov <eugene_batalov@mail.ru>
2017-01-27 16:08:47 +0300

    +params

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,7 @@
 class CoursesController < ApplicationController
-#  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
   def index
-    @search_term = 'java'
+    @search_term = params[:looking_for] || 'jhu'
     @courses = Coursera.for(@search_term)
   end
 end
